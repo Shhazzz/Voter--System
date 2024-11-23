50f2cce03d608926c93c835a2daf25c871ce977d
Sameera677 <ksameera.k123@gmail.com>
2024-11-23 22:47:20 +0530

    I am done

--- T3-Sameera.c
+++ T3-Sameera.c
@@ -1,1 +1,65 @@
+#include <stdio.h>
+int main()
+{
+    int choice, add_candidate, cast_vote, display_result, save_results, party, Nota,
+        Indian_public_Party, National_Conference_Party, Majority_People_Party;
+    while (1)
+    {
+        printf("\n--- Voting System ---\n");
+        printf("1. Add candidate\n");
+        printf("2. Cast Vote\n");
+        printf("3. Display candidate\n");
+        printf("4. Exit\n");
+        printf("Enter your choice:");
+        scanf("%d", &choice);
 
+        switch (choice)
+        {
+        case 1:
+            add_candidate;
+            break;
+        case 2:
+            cast_vote;
+            while (1)
+            {
+                printf("\n--- Choose Party ---\n");
+                printf("1. Indian Public Party\n");
+                printf("2. National Conference Party\n");
+                printf("3. Majority People Party\n");
+                printf("4. Nota\n");
+                printf("Enter your party:");
+                scanf("%d", &party);
+                switch (party)
+                {
+                case 1:
+                    Indian_public_Party;
+                    break;
+                case 2:
+                    National_Conference_Party;
+                    break;
+                case 3:
+                    Majority_People_Party;
+                    break;
+                case 4:
+                    Nota;
+                    break;
+                default:
+                    printf("Invalid Party");
+                    break;
+                    return 0;
+                }
+                break;
+            case 3:
+                display_result;
+                break;
+            case 4:
+                save_results;
+                printf("Exiting the system. Goodbye!\n");
+                return 0;
+            default:
+                printf("Invalid choice. Please try again.\n");
+            }
+        }
+        return 0;
+    }
+}
