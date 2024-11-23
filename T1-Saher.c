f3b50672af008e7245b900d450cb0244cefe06e8
Saher Patel <saherpatel04@gmail.com>
2024-11-23 20:37:58 +0530

    T1-Saher.c uploaded

--- T1-Saher.c
+++ T1-Saher.c
@@ -1,1 +1,15 @@
+ #include<stdio.h>
+ #include<string.h>
+
+ //DECLARE A STRUCTURE FOR VOTERS
+ struct Voter{
+    char name[30],gender,address[50]; //CANDIDATES NAME,GENDER AND ADDRESS
+    int age,ID,contact no[10];        //CANDIDATES AGE,UNIQUE ID,CONTACT NO
+ };
+
+ //DECLARE A STRUCTURE FOR PARTIES
+ struct Party{
+    char name[50],ID; //PARTIES NAME AND ID
+    int votes;       //NUMBER OF VOTES THE PARTY HAS RECEIVED
+ };
 
