d77ee0c9d7a8e0ecceea9cd19cffda5ce0a8394b
Shhazzz <shazzahra.zaidi@gmail.com>
2024-11-24 11:57:03 +0530

    Update T4-Shaz.c

--- T4-Shaz.c
+++ T4-Shaz.c
@@ -28,3 +28,44 @@ int main () {
     }
     return 0;
 }
+/*#include <stdio.h>
+int main () {
+    int i,j=100,a[100],age, n, Vcount1=0,Vcount2=0,Vcount3=0, Vcount4=0,large=0;
+    start:
+    printf("enter number of ppl");
+    scanf("%d", &n);
+     if (n<=0) {
+        printf("Number CANNOT be less than equal to 0 OR a CHARACTER.\n");
+       goto start;
+
+      } else if (n>0) {
+for (int i = 0 ; i < n ; i++) {
+        printf("Please enter your age:\n");
+        scanf("%d", &age);
+            if (age<18) {
+        printf("You're NOT ELIGIBLE  to vote.\n");}
+        printf("enter party number");
+        scanf("%d", &a[i]);
+        j++;
+        printf("Your ID/Serial NO. for reference is %d.\n", j);
+
+        if (a[i] == 1){
+                printf("You have successfully casted your vote to the 1st party.\n\n\a");
+                Vcount1++;
+        } else if (a[i] == 2){
+            printf("You have successfully casted your vote to the 2nd party.\n\n\a");
+            Vcount2++;
+        } else if (a[i] == 3){
+            printf("You have successfully casted your vote to the 3rd party.\n\n\a");
+            Vcount3++;
+        } else if (a[i] == 4){
+            printf("You have successfully casted your vote to NONE OF THE ABOVE parties.\n\n\a");
+            Vcount4++;
+        } else if (a[i]>=5) {
+            printf("Sorry!Option to vote for %dth party doesn't exist.\nPlease cast your votes accordingly!\n\n\a",a[i]);
+        }
+    }}
+    return 0;
+}
+*/
+
