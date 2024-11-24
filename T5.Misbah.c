32c1c60b8ef0dfa4f1d3bb8623deea77a00404d5
misbah5656 <misbahreshamwalla@gmail.com>
2024-11-24 16:01:01 +0530

    T5

--- T5.Misbah.c
+++ T5.Misbah.c
@@ -1,1 +1,12 @@
+// Find the winner
+    int winnerIndex = findWinner(votes, numCandidates);
 
+    // Output the results
+    printf("\nVoting Results:\n");
+    for (int i = 0; i < numCandidates; i++) {
+        printf("Candidate %d: %d votes\n", num, votes[i]);
+    }
+
+    printf("\nThe winner is Candidate %d with %d votes.\n", winnerIndex + 1, votes[winnerIndex]);
+
+    return 0
