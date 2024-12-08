2c63c02076cf26d1fe361c6f856525592d37567d
misbah5656 <misbahreshamwalla@gmail.com>
2024-12-08 12:41:23 +0530

    Uploaded

--- T5.Misbah.c
+++ T5.Misbah.c
@@ -1,12 +1,26 @@
-// Find the winner
-    int winnerIndex = findWinner(votes, numCandidates);
-
-    // Output the results
-    printf("\nVoting Results:\n");
-    for (int i = 0; i < numCandidates; i++) {
-        printf("Candidate %d: %d votes\n", num, votes[i]);
+//MISBAH
+void declare_winner() {
+    //Checking total votes and displaying election winner 
+    int i;
+    if (candidate_count == 0) {
+        printf("No candidates available to determine a winner.\n");
+        return;
     }
-
-    printf("\nThe winner is Candidate %d with %d votes.\n", winnerIndex + 1, votes[winnerIndex]);
-
-    return 0
+    int max_votes = 0;
+    char winner[MAX_NAME_LENGTH];
+    strcpy(winner, "No winner"); // Default if no votes
+ 
+    for (int i = 0 ; i < candidate_count ; i++) {
+        if (candidates[i].votes > max_votes) {
+            max_votes = candidates[i].votes;
+            strcpy(winner, candidates[i].name);
+        }
+    }
+    printf("\n--- Winner of the Election ---\n");
+    if (max_votes > 0) {
+        printf("The winner is %s from %s with a total of %d votes.\n", winner, candidates[i].party, max_votes);
+    }
+    else {
+        printf("No votes have been cast.Hence winner cannot be declared.\n");
+    }
+}
